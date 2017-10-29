fd915bb593723511cd4c0cf12e50266886f2b18e
camargo <camargo@lisha.ufsc.br>
2017-10-28 22:09:00 -0200

    debug message

--- fw/platform/linux/i2c.cpp
+++ fw/platform/linux/i2c.cpp
@@ -136,7 +136,9 @@ uint16_t I2CBus::read_short ( Register reg, bool lendian )
     msgs[1].buf = readbuf;
     msgs[1].len = 2;
     
-    ioctl( p->fd, I2C_RDWR, &opdata );
+    int ret = ioctl( p->fd, I2C_RDWR, &opdata );    
+    if(ret)
+        fprintf(stderr, "READ_SHORT IOCTL ERROR %d" ret);
     
     return lendian? readbuf[0] | (readbuf[1] << 8) : readbuf[1] | (readbuf[0] << 8);
 }
